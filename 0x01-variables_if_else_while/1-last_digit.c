63611882585802daf1dda259d0a344da15351023
Aisha Rashid <aisha.wrashid64@gmail.com>
2022-03-11 02:35:14 -0800

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,11 @@
 
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
